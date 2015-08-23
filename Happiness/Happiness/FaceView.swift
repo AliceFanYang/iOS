96c6f69854d76d62fc7ea013d9c55a7a0dbf92c7
AliceFanYang <alice.f.yang@gmail.com>
2015-08-23 18:05:41 -0400

    added gestures

--- Happiness/Happiness/FaceView.swift
+++ Happiness/Happiness/FaceView.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
+//protocol FaceViewDataSource: class {
+//    func setSmilinessForFaceView(self) -> Double? {
+    
+//    }
+//}
+
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
 class FaceView: UIView {
 
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet{ setNeedsDisplay() } }
+    @IBInspectable
     var color: UIColor = UIColor.blueColor() { didSet{ setNeedsDisplay() } }
+    @IBInspectable
+    var scale: CGFloat = 0.9 { didSet{ setNeedsDisplay() } }
     
     var faceCenter: CGPoint {
         return convertPoint(center, fromView: superview)
     }
     
     var faceRadius: CGFloat {
-        return min(bounds.size.width, bounds.size.height) / 2 * 0.90
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
+    weak var dataSource: FaceViewDataSource?
     
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
@@ -72,7 +89,7 @@ class FaceView: UIView {
         mouthPath.lineWidth = lineWidth
         return mouthPath
     }
-
+    
     override func drawRect(rect: CGRect) {
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: true)
         color.set()
@@ -82,8 +99,15 @@ class FaceView: UIView {
         bezierPathForEye(.Left).stroke()
         bezierPathForEye(.Right).stroke()
         
-        let smiliness = 0.75
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
         let smilePath = bezierPathForSmile(smiliness)
         smilePath.stroke()
+    }
+    
+    func scale(gesture: UIPinchGestureRecognizer) {
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
     }
 }
